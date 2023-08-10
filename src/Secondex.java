e8f70e7f426812ef23581cee2eb459d046850e7a
Aleksandra <raspa03@mail.ru>
2023-08-10 15:55:29 +0700

    Edit

--- src/Secondex.java
+++ src/Secondex.java
@@ -1,3 +1,12 @@
 public class Secondex {
-
+    public static void main(String[] args) {
+        for (int i = 100; i>=1; i--){
+            if (checkNumber(i) == true) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static boolean checkNumber(int a){
+        return (a % 4 == 0);
+    }
 }
---
+++ src/Thirdexc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Thirdexc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = scanner.nextInt();
+        int sumDidgits = 0;
+        int oddDidgits = 0;
+        int maxDigit = 0;
+        while (a > 0) {
+            sumDidgits = sumDidgits + a % 10;
+            if (checkNumber(a%10)) {
+                oddDidgits = oddDidgits + a%10;
+            }
+            if (a%10 >= maxDigit) {
+                maxDigit = a%10;
+            }
+            a /= 10;
+        }
+        System.out.println("Сумма цифр числа = " + sumDidgits);
+        System.out.println("Сумма нечетных цифр числа = " + oddDidgits);
+        System.out.println("Максимальная цифра числа = " + maxDigit);
+
+    }
+    public static boolean checkNumber(int a) {
+        return (a % 2 != 0);
+    }
+}
